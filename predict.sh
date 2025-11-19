fd2fccba3f5cc0d1adf555eb3cba7080724ac34c
gloriagrama <130309591+gloriagrama@users.noreply.github.com>
2025-11-19 11:11:49 -0700

    Modify SLURM script for user-specific paths
    Updated job array range and adjusted time limit. Changed input and output directories to use a user-specific path.

--- predict.sh
+++ predict.sh
@@ -2,14 +2,14 @@
 #SBATCH --job-name=alphafold3_array
 #SBATCH --output=alphafold_slurm/alphafold3_%A_%a.out
 #SBATCH --error=alphafold_slurm/alphafold3_%A_%a.err
-#SBATCH --array=1-7200
+#SBATCH --array=1-{# of inputs}
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=32G
 #SBATCH --partition=htc
 #SBATCH --qos=public
 #SBATCH --gres=gpu:1
-#SBATCH --time=00:45:00
+#SBATCH --time=01:30:00
 #SBATCH --export=NONE
 
 # Load CUDA (must match container build)
@@ -17,9 +17,9 @@ module load cuda-12.6.1-gcc-12.1.0
 
 # Paths
 SIMG=/packages/apps/simg/alphafold-3.0.0.sif
-AF3_INPUT_DIR=/scratch/ggrama/recomb/generated/af_inputs
-AF3_OUTPUT_DIR=/scratch/ggrama/recomb/generated/af_outputs
-MODEL_DIR=/scratch/ggrama/alphafold3/params
+AF3_INPUT_DIR=/scratch/{USER}/alphafold/af_inputs
+AF3_OUTPUT_DIR=/scratch/{USER}/alphafold/af_outputs
+MODEL_DIR=/scratch/ggrama/alphafold3/params #or I can share the parameters with you
 DB_DIR=/scratch/ggrama/alphafold3/af3_database
 
 mkdir -p $AF3_OUTPUT_DIR/logs
